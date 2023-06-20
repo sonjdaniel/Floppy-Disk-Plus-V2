17a39678b57b5ca2114fac4cd60911cebdc1a45a
sonjdaniel <sonjdaniel@gmail.com>
2023-06-20 18:09:55 -0500

    Starter for Game.js

---
+++ models/Game.js
@@ -0,0 +1,14 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Game extends Model {}
+
+Game.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+})
