aa27de309b62d4b7995d0da5ff917bddfa437fc0
Ruby <rubyfredrickson@gmail.com>
2023-06-21 18:35:24 -0500

    fixed up code

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,22 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
+// Using Node.js 14.x +
+// use "lib" package from npm
+const lib = require('lib')({token: 'tok_dev_6fxHjqhTnmm8XxyqsnUgYp1VeaxSEAFQTdXRGDBNHaUqf23jCGGeASHH2SeCWSsc'});
+
+// make API request
+
+async function fuseThatPokemon(){
+let result = await lib.keith.pokefusion['@0.2.0']({
+    headPokemon: `pikachu`,
+    bodyPokemon: `magikarp`
+  });
+    console.log(result)
+}
+
+fuseThatPokemon;
+
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
--- models/index.js
+++ models/index.js
@@ -1,39 +1,39 @@
 //Starting code
-const User = require("./User");
-const Game = require("./Game");
-const Level = require("./Level");
-const Monster = require("./Monster");
-const Pokemon = require("./Pokemon");
+// const User = require("./User");
+// const Game = require("./Game");
+// const Level = require("./Level");
+// const Monster = require("./Monster");
+// const Pokemon = require("./Pokemon");
 
-User.hasMany(Pokemon, {
-  foreignKey: 'user_id',
-});
+// User.hasMany(Pokemon, {
+//   foreignKey: 'user_id',
+// });
 
-User.hasMany(Game, {
-  foreignKey: 'user_id',
-});
+// User.hasMany(Game, {
+//   foreignKey: 'user_id',
+// });
 
-Pokemon.belongsTo(User, {
-  foreignKey: 'user_id',
-});
+// Pokemon.belongsTo(User, {
+//   foreignKey: 'user_id',
+// });
 
-Level.hasMany(Monster, {
-  foreignKey: 'level_id',
-});
+// Level.hasMany(Monster, {
+//   foreignKey: 'level_id',
+// });
 
-Monster.belongsTo(Level, {
-  foreignKey: 'level_id',
-});
+// Monster.belongsTo(Level, {
+//   foreignKey: 'level_id',
+// });
 
-Level.belongsTo(Game, {
-  foreignKey: 'game_id',
-});
+// Level.belongsTo(Game, {
+//   foreignKey: 'game_id',
+// });
 
-Game.belongsTo(User, {
-  foreignKey: 'user_id',
-});
+// Game.belongsTo(User, {
+//   foreignKey: 'user_id',
+// });
 
-Game.hasMany(Level,{
-  foreignKey: 'game_id',
-})
-module.exports = { User, Game, Level, Monster, Pokemon };
+// Game.hasMany(Level,{
+//   foreignKey: 'game_id',
+// })
+// module.exports = { User, Game, Level, Monster, Pokemon };
